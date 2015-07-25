1facd7351e40bdb3aadfac497d6208b11eaecf3e
1000ch <shogo.sensui@gmail.com>
2015-07-25 14:43:09 +0900

    update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use babel';
 
-import { CompositeDisposable } from 'atom';
+import { Range }     from 'atom';
 import postcss       from 'postcss';
-import AtomLinter    from 'atom-linter';
 import stylelint     from 'stylelint';
-import configSuitcss from 'stylelint-config-suitcss';
+
+import configSuitcss    from 'stylelint-config-suitcss';
+import configCssRecipes from 'stylelint-config-cssrecipes';
 
 export let config = {
   usePreset: {
@@ -18,24 +19,12 @@ export let config = {
     description: 'Select lint style if you select from presets',
     type: 'string',
     default: 'stylelint-config-suitcss',
-    enum: ['stylelint-config-suitcss']
+    enum: ['stylelint-config-suitcss', 'stylelint-config-cssrecipes']
   }
 };
 
 const usePreset    = () => atom.config.get('linter-stylelint.usePreset');
 const presetConfig = () => atom.config.get('linter-stylelint.presetConfig');
-
-export let subscriptions = null;
-
-export const activate = () => {
-  this.subscriptions = new CompositeDisposable();
-};
-
-export const deactivate = () => {
-  if (this.subscriptions) {
-    this.subscriptions.dispose();
-  }
-};
 
 export const provideLinter = () => {
 
@@ -58,20 +47,22 @@ export const provideLinter = () => {
           from: path
         }).then((data) => {
 
-          console.log(data);
+          resolve(data.messages.map((message) => {
 
-          let errors = data.messages.map((message) => {
+            let start = message.node.source.start;
+            let end   = message.node.source.end;
+            let range = new Range(
+              [start.line - 1, start.column - 1],
+              [end.line - 1, end.column - 1]
+            );
+
             return {
               type: message.type,
               text: message.text,
               filePath: path,
-              range: [[1, 1]]
+              range: range
             }
-          });
-
-          console.log(errors);
-
-          resolve(errors);
+          }));
 
         }).catch((error) => console.error(error));
       });
