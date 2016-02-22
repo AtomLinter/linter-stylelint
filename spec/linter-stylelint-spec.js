c6e6b095c7f23fed95f344aa9c9dba871364fadd
1000ch <shogo.sensui@gmail.com>
2016-02-23 01:37:28 +0900

    Revert code styles

--- spec/linter-stylelint-spec.js
+++ spec/linter-stylelint-spec.js
@@ -11,29 +11,25 @@ describe('The stylelint provider for Linter', () => {
     atom.config.set('linter-stylelint.disableWhenNoConfig', false);
 
     waitsForPromise(() =>
-      atom
-        .packages
-        .activatePackage('linter-stylelint')
-        .then(() => atom.packages.activatePackage('language-css'))
+      Promise.all([
+        atom.packages.activatePackage('linter-stylelint'),
+        atom.packages.activatePackage('language-css')
+      ])
     );
   });
 
   it('detects invalid coding style in bad.css and report as error', () => {
     waitsForPromise(() => {
       const bad = path.join(__dirname, 'fixtures', 'bad', 'bad.css');
-      return atom
-        .workspace
-        .open(bad)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toBeGreaterThan(0);
+      return atom.workspace.open(bad).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toBeGreaterThan(0);
 
-          // test only the first error
-          expect(messages[0].type).toEqual('Error');
-          expect(messages[0].text).toEqual('Unexpected empty block (block-no-empty)');
-          expect(messages[0].filePath).toMatch(/.+bad\.css$/);
-          expect(messages[0].range).toEqual([[0, 5], [0, 7]]);
-        });
+        // test only the first error
+        expect(messages[0].type).toEqual('Error');
+        expect(messages[0].text).toEqual('Unexpected empty block (block-no-empty)');
+        expect(messages[0].filePath).toMatch(/.+bad\.css$/);
+        expect(messages[0].range).toEqual([[0, 5], [0, 7]]);
+      });
     });
   });
 
@@ -42,50 +38,38 @@ describe('The stylelint provider for Linter', () => {
 
     waitsForPromise(() => {
       const warn = path.join(__dirname, 'fixtures', 'warn', 'warn.css');
-      return atom
-        .workspace
-        .open(warn)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toBeGreaterThan(0);
+      return atom.workspace.open(warn).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toBeGreaterThan(0);
 
-          // test only the first error
-          expect(messages[0].type).toEqual('Warning');
-          expect(messages[0].text).toEqual('Unexpected empty block (block-no-empty)');
-          expect(messages[0].filePath).toMatch(/.+warn\.css$/);
-          expect(messages[0].range).toEqual([[0, 5], [0, 7]]);
-        });
+        // test only the first error
+        expect(messages[0].type).toEqual('Warning');
+        expect(messages[0].text).toEqual('Unexpected empty block (block-no-empty)');
+        expect(messages[0].filePath).toMatch(/.+warn\.css$/);
+        expect(messages[0].range).toEqual([[0, 5], [0, 7]]);
+      });
     });
   });
 
   it('finds nothing wrong with a valid file (good.css)', () => {
     waitsForPromise(() => {
       const good = path.join(__dirname, 'fixtures', 'good', 'good.css');
-      return atom
-        .workspace
-        .open(good)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(0);
-        });
+      return atom.workspace.open(good).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(0);
+      });
     });
   });
 
   it('show CSS syntax error with an invalid file (invalid.css)', () => {
     waitsForPromise(() => {
       const invalid = path.join(__dirname, 'fixtures', 'invalid', 'invalid.css');
-      return atom
-        .workspace
-        .open(invalid)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(1);
+      return atom.workspace.open(invalid).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(1);
 
-          expect(messages[0].type).toEqual('Error');
-          expect(messages[0].text).toEqual('Unknown word');
-          expect(messages[0].filePath).toMatch(/.+invalid\.css$/);
-          expect(messages[0].range).toEqual([[0, 0], [0, 3]]);
-        });
+        expect(messages[0].type).toEqual('Error');
+        expect(messages[0].text).toEqual('Unknown word');
+        expect(messages[0].filePath).toMatch(/.+invalid\.css$/);
+        expect(messages[0].range).toEqual([[0, 0], [0, 3]]);
+      });
     });
   });
 
@@ -94,19 +78,15 @@ describe('The stylelint provider for Linter', () => {
 
     waitsForPromise(() => {
       const invalid = path.join(__dirname, 'fixtures', 'invalid-rule', 'styles.css');
-      return atom
-        .workspace
-        .open(invalid)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(1);
+      return atom.workspace.open(invalid).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(1);
 
-          expect(messages[0].type).toEqual('Error');
-          const text = 'Unexpected option value "foo" for rule "block-no-empty"';
-          expect(messages[0].text).toEqual(text);
-          expect(messages[0].filePath).toMatch(/.+styles\.css$/);
-          expect(messages[0].range).toEqual([[0, 0], [0, 6]]);
-        });
+        expect(messages[0].type).toEqual('Error');
+        const text = 'Unexpected option value "foo" for rule "block-no-empty"';
+        expect(messages[0].text).toEqual(text);
+        expect(messages[0].filePath).toMatch(/.+styles\.css$/);
+        expect(messages[0].range).toEqual([[0, 0], [0, 6]]);
+      });
     });
   });
 
@@ -116,20 +96,16 @@ describe('The stylelint provider for Linter', () => {
 
     waitsForPromise(() => {
       const invalid = path.join(__dirname, 'fixtures', 'invalid-extends', 'styles.css');
-      return atom
-        .workspace
-        .open(invalid)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(0);
+      return atom.workspace.open(invalid).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(0);
 
-          const addError = atom.notifications.addError;
-          const args = addError.mostRecentCall.args;
-          expect(addError.calls.length).toEqual(1);
-          expect(args[0]).toEqual('Unable to run stylelint');
-          expect(args[1].detail).toContain('Could not find "some-module-that-will-never-exist".');
-          expect(args[1].dismissable).toEqual(true);
-        });
+        const addError = atom.notifications.addError;
+        const args = addError.mostRecentCall.args;
+        expect(addError.calls.length).toEqual(1);
+        expect(args[0]).toEqual('Unable to run stylelint');
+        expect(args[1].detail).toContain('Could not find "some-module-that-will-never-exist".');
+        expect(args[1].dismissable).toEqual(true);
+      });
     });
   });
 
@@ -139,20 +115,16 @@ describe('The stylelint provider for Linter', () => {
 
     waitsForPromise(() => {
       const invalid = path.join(__dirname, 'fixtures', 'invalid-config', 'styles.css');
-      return atom
-        .workspace
-        .open(invalid)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(0);
+      return atom.workspace.open(invalid).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(0);
 
-          const addError = atom.notifications.addError;
-          const args = addError.mostRecentCall.args;
-          expect(addError.calls.length).toEqual(1);
-          expect(args[0]).toEqual('Unable to parse stylelint configuration');
-          expect(args[1].detail).toContain('>>>');
-          expect(args[1].dismissable).toEqual(true);
-        });
+        const addError = atom.notifications.addError;
+        const args = addError.mostRecentCall.args;
+        expect(addError.calls.length).toEqual(1);
+        expect(args[0]).toEqual('Unable to parse stylelint configuration');
+        expect(args[1].detail).toContain('>>>');
+        expect(args[1].dismissable).toEqual(true);
+      });
     });
   });
 
@@ -162,14 +134,10 @@ describe('The stylelint provider for Linter', () => {
 
     waitsForPromise(() => {
       const bad = path.join(__dirname, 'fixtures', 'bad', 'bad.css');
-      return atom
-        .workspace
-        .open(bad)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(0);
-          expect(atom.notifications.addError.calls.length).toEqual(0);
-        });
+      return atom.workspace.open(bad).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(0);
+        expect(atom.notifications.addError.calls.length).toEqual(0);
+      });
     });
   });
 
@@ -178,14 +146,10 @@ describe('The stylelint provider for Linter', () => {
 
     waitsForPromise(() => {
       const ignore = path.join(__dirname, 'fixtures', 'ignore-files', 'styles.css');
-      return atom
-        .workspace
-        .open(ignore)
-        .then(editor => lint(editor))
-        .then(messages => {
-          expect(messages.length).toEqual(0);
-          expect(atom.notifications.addError.calls.length).toEqual(0);
-        });
+      return atom.workspace.open(ignore).then(editor => lint(editor)).then(messages => {
+        expect(messages.length).toEqual(0);
+        expect(atom.notifications.addError.calls.length).toEqual(0);
+      });
     });
   });
 });
