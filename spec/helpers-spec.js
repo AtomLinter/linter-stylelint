ed1a3fe9c2e04ed340a74773dd3d2c483ffa5828
Joshua Kiwiet-Pantaleoni <emuvente@gmail.com>
2017-12-10 17:48:46 -0800

    Use the stylelint instance local to the project if one exists

---
+++ spec/helpers-spec.js
@@ -0,0 +1,39 @@
+'use babel';
+
+import * as path from 'path';
+// eslint-disable-next-line no-unused-vars, import/no-extraneous-dependencies
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
+import * as helpers from '../lib/helpers';
+
+const fixtures = path.join(__dirname, 'fixtures');
+
+describe('Helpers', () => {
+  describe('findStylelintDirectory', () => {
+    it('resturns a string', () => {
+      const modulesDir = path.join(fixtures, 'local-stylelint', 'node_modules');
+      const foundStylelint = helpers.findStylelintDirectory(modulesDir);
+      expect(typeof foundStylelint === 'string').toBe(true);
+    });
+
+    it('finds a local stylelint when available', () => {
+      const modulesDir = path.join(fixtures, 'local-stylelint', 'node_modules');
+      const foundStylelint = helpers.findStylelintDirectory(modulesDir);
+      const expectedStylelintPath = path.join(fixtures, 'local-stylelint', 'node_modules', 'stylelint');
+      expect(foundStylelint).toEqual(expectedStylelintPath);
+    });
+
+    it('falls back to the packaged stylelint when no local stylelint is found', () => {
+      const modulesDir = 'not/a/real/path';
+      const foundStylelint = helpers.findStylelintDirectory(modulesDir);
+      const expectedBundledPath = path.join(__dirname, '..', 'node_modules', 'stylelint');
+      expect(foundStylelint).toEqual(expectedBundledPath);
+    });
+  });
+
+  describe('getStylelintInstance && getStylelintFromDirectory', () => {
+    it('tries to find a local stylelint', () => {
+      const stylelint = helpers.getStylelintInstance(path.join(fixtures, 'local-stylelint', 'good.css'));
+      expect(stylelint).toBe('located');
+    });
+  });
+});
