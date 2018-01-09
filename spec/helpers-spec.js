5873fccb4a968cd3399467d3aaeaf940b10c5c13
Joshua Kiwiet-Pantaleoni <emuvente@gmail.com>
2018-01-09 11:19:34 -0800

    Use async resolve; Cache resolved stylelint paths by project directory

--- spec/helpers-spec.js
+++ spec/helpers-spec.js
@@ -5,27 +5,28 @@ import path from 'path';
 import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
 import * as helpers from '../lib/helpers';
 
-const fixtures = path.join(__dirname, 'fixtures');
+const fixturesLocal = path.join(__dirname, 'fixtures', 'local-stylelint');
 
 describe('Helpers', () => {
   describe('getStylelintPath', () => {
-    it('finds a local stylelint when available', () => {
-      const foundStylelint = helpers.getStylelintPath(path.join(fixtures, 'local-stylelint', 'good.css'));
-      const expectedStylelintPath = path.join(fixtures, 'local-stylelint', 'node_modules', 'stylelint', 'lib', 'index.js');
+    it('finds a local stylelint when available', async () => {
+      const foundStylelint = await helpers.getStylelintPath(fixturesLocal);
+      const expectedStylelintPath = path.join(fixturesLocal, 'node_modules', 'stylelint', 'lib', 'index.js');
       expect(foundStylelint).toBe(expectedStylelintPath);
     });
 
-    it('falls back to the packaged stylelint when no local stylelint is found', () => {
+    it('falls back to the packaged stylelint when no local stylelint is found', async () => {
       const filePath = 'not/a/real/path';
-      const foundStylelint = helpers.getStylelintPath(filePath);
+      const foundStylelint = await helpers.getStylelintPath(filePath);
       const expectedBundledPath = require.resolve('stylelint');
       expect(foundStylelint).toBe(expectedBundledPath);
     });
   });
 
   describe('getStylelintInstance', () => {
-    it('tries to find a local stylelint', () => {
-      const stylelint = helpers.getStylelintInstance(path.join(fixtures, 'local-stylelint', 'good.css'));
+    it('tries to find a local stylelint', async () => {
+      atom.project.addPath(fixturesLocal);
+      const stylelint = await helpers.getStylelintInstance(path.join(fixturesLocal, 'good.css'));
       expect(stylelint).toBe('located');
     });
   });
