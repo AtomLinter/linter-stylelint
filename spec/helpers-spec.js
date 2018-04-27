d8f9bf661657fcb1dec4caa838b7605033deb188
Joshua Kiwiet-Pantaleoni <emuvente@gmail.com>
2018-04-27 16:02:20 -0700

    feat: use the stylelint instance local to the project if one exists (#381)
    * Use the stylelint instance local to the project if one exists
    * Remove unparseable (and apparently unneeded) test package.json file
    * Helpers test cleanup
    * Use require.resolve to find path of packaged stylelint
    * Cache stylelint instances between linting runs
    * Restore original NODE_PATH after stylelint completes
    * Use a Map for the module cache
    * Use browserify/resolve to find local stylelint instance
    * Use async resolve; Cache resolved stylelint paths by project directory

---
+++ spec/helpers-spec.js
@@ -0,0 +1,33 @@
+'use babel';
+
+import path from 'path';
+// eslint-disable-next-line no-unused-vars, import/no-extraneous-dependencies
+import { it, fit, wait, beforeEach, afterEach } from 'jasmine-fix';
+import * as helpers from '../lib/helpers';
+
+const fixturesLocal = path.join(__dirname, 'fixtures', 'local-stylelint');
+
+describe('Helpers', () => {
+  describe('getStylelintPath', () => {
+    it('finds a local stylelint when available', async () => {
+      const foundStylelint = await helpers.getStylelintPath(fixturesLocal);
+      const expectedStylelintPath = path.join(fixturesLocal, 'node_modules', 'stylelint', 'lib', 'index.js');
+      expect(foundStylelint).toBe(expectedStylelintPath);
+    });
+
+    it('falls back to the packaged stylelint when no local stylelint is found', async () => {
+      const filePath = 'not/a/real/path';
+      const foundStylelint = await helpers.getStylelintPath(filePath);
+      const expectedBundledPath = require.resolve('stylelint');
+      expect(foundStylelint).toBe(expectedBundledPath);
+    });
+  });
+
+  describe('getStylelintInstance', () => {
+    it('tries to find a local stylelint', async () => {
+      atom.project.addPath(fixturesLocal);
+      const stylelint = await helpers.getStylelintInstance(path.join(fixturesLocal, 'good.css'));
+      expect(stylelint).toBe('located');
+    });
+  });
+});
