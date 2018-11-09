cf83fac2249b44306a7f7528b6935eade3c5e9b3
Ian VanSchooten <ian.vanschooten@gmail.com>
2018-11-08 23:30:12 -0500

    test: add specs for applyFixedStyles

--- spec/helpers-spec.js
+++ spec/helpers-spec.js
@@ -7,7 +7,11 @@ import {
 } from 'jasmine-fix';
 import * as helpers from '../lib/helpers';
 
-const fixturesLocal = path.join(__dirname, 'fixtures', 'local-stylelint');
+const fixtures = path.join(__dirname, 'fixtures');
+const fixturesLocal = path.join(fixtures, 'local-stylelint');
+const autofixableBadFilePath = path.join(fixtures, 'autofix', 'bad.css');
+const autofixableGoodFilePath = path.join(fixtures, 'autofix', 'good.css');
+
 
 describe('Helpers', () => {
   describe('getStylelintPath', () => {
@@ -30,6 +34,53 @@ describe('Helpers', () => {
       atom.project.addPath(fixturesLocal);
       const stylelint = await helpers.getStylelintInstance(path.join(fixturesLocal, 'good.css'));
       expect(stylelint).toBe('located');
+    });
+  });
+
+  describe('applyFixedStyles', () => {
+    const fixedText = `.style {
+    color: blue;
+    margin: 0;
+}
+`;
+    const mockResults = {
+      _postcssResult: {
+        root: {
+          toString: () => fixedText
+        },
+        opts: {
+          syntax: null // Just need the right shape, we're faking our toString
+        }
+      }
+    };
+
+    beforeEach(async () => {
+      atom.workspace.destroyActivePaneItem();
+    });
+
+    it('sets the editor text to the linting results', async () => {
+      const editor = await atom.workspace.open(autofixableBadFilePath);
+      await helpers.applyFixedStyles(editor, mockResults);
+      expect(editor.getText()).toBe(fixedText);
+      expect(editor.isModified()).toBe(true);
+    });
+
+    it('does not change the editor if results match the existing text', async () => {
+      const editor = await atom.workspace.open(autofixableGoodFilePath);
+      await helpers.applyFixedStyles(editor, mockResults);
+      expect(editor.getText()).toBe(fixedText);
+      expect(editor.isModified()).toBe(false);
+    });
+
+    it('restores multiple cursors to their positions if possible', async () => {
+      const editor = await atom.workspace.open(autofixableBadFilePath);
+      const firstCursorPosition = editor.getCursorBufferPosition();
+      const secondCursorPosition = [1, 0];
+      editor.addCursorAtBufferPosition(secondCursorPosition);
+      await helpers.applyFixedStyles(editor, mockResults);
+      const actualBufferPositions = editor.getCursorBufferPositions();
+      expect(actualBufferPositions.length).toBe(2);
+      expect(actualBufferPositions).toEqual([firstCursorPosition, secondCursorPosition]);
     });
   });
 });
